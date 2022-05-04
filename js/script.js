728af2d2a195c9f0a21eb1305a4fd512eb5574ad
jpablux <mail@jpabloestrada.com>
2022-05-03 18:31:27 -0600

    added changes to responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 window.onload = function(){
    const menu_btn = document.querySelector('.hamburger');
    const mobile_menu = document.querySelector('.menu_container');
+   const mobile_logo = document.querySelector('.logo_container');
+   const nav_mobile = document.querySelector('.navbar-mobile');
    
    menu_btn.addEventListener('click', function(){
        menu_btn.classList.toggle('is-active');
        mobile_menu.classList.toggle('is-active');
+       mobile_logo.classList.toggle('is-active');
+       nav_mobile.classList.toggle('is-active');
    });
 }
